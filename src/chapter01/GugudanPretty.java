25a308f3cfe31f40618b8710adb1753b0586e8f8
maenttang <36221352+maenttang@users.noreply.github.com>
2018-11-22 16:16:27 +0900

    문제풀이-트라이앵글, 구구단

--- src/chapter01/GugudanPretty.java
+++ src/chapter01/GugudanPretty.java
@@ -3,7 +3,13 @@ package chapter01;
 public class GugudanPretty {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		for (int j = 1; j <= 9; j++) {
+			for (int i = 1; i <= 9; i++) {
+				System.out.print(i + "x"+ j+"="+(j*i)+"\t");
+			}
+			System.out.println("");
+		}
 
 	}
 
--- src/chapter01/Triangle.java
+++ src/chapter01/Triangle.java
@@ -4,13 +4,13 @@ public class Triangle {
 
 	public static void main(String[] args) {
 
-		for (int j = 0; j < 5; j++) {
-			for (int i = 0; i < 6; i++) {
+		for (int j = 1; j <= 5; j++) {
+			for (int i = 0; i < j; i++) {
 				System.out.print("X");
 			}
-			System.out.println();
+			System.out.println("");
+
 		}
-		
 	}
 
 }
